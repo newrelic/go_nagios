ed7f94aad784b56176b78f08dc314971896ff477
Karl Matthias <kmatthias@newrelic.com>
2014-05-02 09:34:34 -0700

    Added appropriate comments for documentation.

--- go_nagios.go
+++ go_nagios.go
@@ -7,6 +7,7 @@ import (
 
 type NagiosStatusVal int
 
+// The values with which a Nagios check can exit
 const (
 	NAGIOS_OK NagiosStatusVal = iota
 	NAGIOS_WARNING
@@ -14,6 +15,7 @@ const (
 	NAGIOS_UNKNOWN
 )
 
+// Maps the NagiosStatusVal entries to output strings
 var (
 	valMessages = []string{
 		"OK:",
@@ -23,11 +25,15 @@ var (
 	}
 )
 
+// A type representing a Nagios check status. The Value is a the exit code
+// expected for the check and the Message is the specific output string.
 type NagiosStatus struct {
 	Message string
 	Value NagiosStatusVal
 }
 
+// Take a bunch of NagiosStatus pointers and find the highest value, then
+// combine all the messages. Things win in the order of highest to lowest.
 func (status *NagiosStatus) Aggregate(otherStatuses []*NagiosStatus) {
 	for _, s := range(otherStatuses) {
 		if status.Value < s.Value {
@@ -38,22 +44,27 @@ func (status *NagiosStatus) Aggregate(otherStatuses []*NagiosStatus) {
 	}
 }
 
+// Exit with an UNKNOWN status and appropriate message
 func Unknown(output string) {
 	ExitWithStatus(&NagiosStatus{output, NAGIOS_UNKNOWN})
 }
 
+// Exit with an CRITICAL status and appropriate message
 func Critical(err error) {
 	ExitWithStatus(&NagiosStatus{err.Error(), NAGIOS_CRITICAL})
 }
 
+// Exit with an WARNING status and appropriate message
 func Warning(output string) {
 	ExitWithStatus(&NagiosStatus{output, NAGIOS_WARNING})
 }
 
+// Exit with an OK status and appropriate message
 func Ok(output string) {
 	ExitWithStatus(&NagiosStatus{output, NAGIOS_OK})
 }
 
+// Exit with a particular NagiosStatus
 func ExitWithStatus(status *NagiosStatus) {
 	fmt.Fprintln(os.Stdout, valMessages[status.Value], status.Message)
 	os.Exit(int(status.Value))
